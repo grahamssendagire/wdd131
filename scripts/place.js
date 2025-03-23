df5c0256bbd5bfc36903122ad0c622d52ea93ae7
grahamssendagire <sse23002@byui.edu>
2025-03-23 19:45:46 +0300

    new things

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,9 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();  
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;  
+const windChillElement = document.getElementById('windChill');  
+const temperature = 5; // Static temperature value for calculations (°C)  
+const windSpeed = 15; // Static wind speed value (km/h)  
+
 function calculateWindChill(temp, speed) {  
     return Math.round(13.12 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));  
 }  
